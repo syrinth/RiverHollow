2ac8018eda71130c7c7504218e9328940e3641c5
Evan Ouellette <evan.ouell@gmail.com>
2017-08-22 20:56:02 -0400

    Abstracted textwindow
    Made GUIDialog into a GUIWindow and GUITextWindo that uses GUIWindow,
    better usability.

--- Game Managers/GUIComponents/GUIObjects/GUITextWindow.cs
+++ Game Managers/GUIComponents/GUIObjects/GUITextWindow.cs
@@ -6,12 +6,10 @@ using System.Collections.Generic;
 
 namespace Adventure.Game_Managers.GUIComponents.GUIObjects
 {
-    public class GUIDialog : GUIObject
+    public class GUITextWindow : GUIObject
     {
+        private GUIWindow _window;
         private SpriteFont _font;
-        private GUIImage[] _ninePatches;
-        private int _midWidth;
-        private int _midHeight;
         private string _text;
 
         string typedText = string.Empty;
@@ -24,24 +22,23 @@ namespace Adventure.Game_Managers.GUIComponents.GUIObjects
         int _currentParsedString = 0;
         public bool _pause = false;
 
-        public GUIDialog(string text)
+        public GUITextWindow(string text)
         {
-            _position = new Vector2(AdventureGame.ScreenWidth/4, AdventureGame.ScreenHeight-180);
-            _width = AdventureGame.ScreenWidth / 2;
-            _ninePatches = new GUIImage[9];
+            _window = new GUIWindow();
+            _position = _window.Position;
+            _width = _window.Width;
+
             _text = text;
             _texture = GameContentManager.GetTexture(@"Textures\Dialog");
             _font = GameContentManager.GetFont(@"Fonts\Font");
             _parsedStrings = new List<string>();
 
             _height = 148;
-            _midWidth = _width - 64;
-            _midHeight = _height - 64;
             delayInMilliseconds = 50;
 
             parseText(text);
         }
-        public GUIDialog(Vector2 position, string text) : this(text)
+        public GUITextWindow(Vector2 position, string text) : this(text)
         {
             _position = position;
         }
@@ -101,7 +98,7 @@ namespace Adventure.Game_Managers.GUIComponents.GUIObjects
             {
                 Vector2 measure = _font.MeasureString(line + word);
 
-                if (measure.Length() > _midWidth)
+                if (measure.Length() > _window.MiddleWidth)
                 {
                     returnString = returnString + line + '\n';
                     line = string.Empty;
@@ -111,7 +108,7 @@ namespace Adventure.Game_Managers.GUIComponents.GUIObjects
                 grabLast = true;
                 line = line + word + ' ';
 
-                if (measure.Y * numReturns > _midHeight)
+                if (measure.Y * numReturns > _window.MiddleHeight)
                 {
                     grabLast = false;
                     _parsedStrings.Add(returnString);
@@ -128,19 +125,7 @@ namespace Adventure.Game_Managers.GUIComponents.GUIObjects
 
         public override void Draw(SpriteBatch spritebatch)
         {
-            {
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X, (int)_position.Y, 32, 32), new Rectangle(0, 0, 32, 32), Color.White);
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X + 32, (int)_position.Y, _midWidth, 32), new Rectangle(32, 0, 32, 32), Color.White);
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X + _midWidth + 32, (int)_position.Y, 32, 32), new Rectangle(64, 0, 32, 32), Color.White);
-
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X, (int)_position.Y + 32, 32, _midHeight), new Rectangle(0, 32, 32, 32), Color.White);
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X + 32, (int)_position.Y + 32, _midWidth, _midHeight), new Rectangle(32, 32, 32, 32), Color.White);
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X + _midWidth + 32, (int)_position.Y + 32, 32, _midHeight), new Rectangle(64, 32, 32, 32), Color.White);
-
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X, (int)_position.Y + _midHeight + 32, 32, 32), new Rectangle(0, 64, 32, 32), Color.White);
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X + 32, (int)_position.Y + _midHeight + 32, _midWidth, 32), new Rectangle(32, 64, 32, 32), Color.White);
-                spritebatch.Draw(_texture, new Rectangle((int)_position.X + _midWidth + 32, (int)_position.Y + _midHeight + 32, 32, 32), new Rectangle(64, 64, 32, 32), Color.White);
-            }
+            _window.Draw(spritebatch);
             spritebatch.DrawString(_font, typedText, new Vector2(_position.X+16, _position.Y+16), Color.White);
         }
     }
---
+++ Game Managers/GUIComponents/GUIObjects/GUIWindow.cs
@@ -0,0 +1,50 @@
+﻿using Adventure.Game_Managers.GUIObjects;
+using Adventure.GUIObjects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace Adventure.Game_Managers.GUIComponents.GUIObjects
+{
+    public class GUIWindow : GUIObject
+    {
+        private GUIImage[] _ninePatches;
+        private int _midWidth;
+        public int MiddleWidth { get => _midWidth; }
+        private int _midHeight;
+        public int MiddleHeight{ get => _midHeight; }
+
+        public GUIWindow()
+        {
+            _position = new Vector2(AdventureGame.ScreenWidth / 4, AdventureGame.ScreenHeight - 180);
+            _width = AdventureGame.ScreenWidth / 2;
+            _ninePatches = new GUIImage[9];
+            _texture = GameContentManager.GetTexture(@"Textures\Dialog");
+
+            _height = 148;
+            _midWidth = _width - 64;
+            _midHeight = _height - 64;
+        }
+        public GUIWindow(Vector2 position)
+        {
+            _position = position;
+        }
+
+        public override void Draw(SpriteBatch spritebatch)
+        {
+            {
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X, (int)_position.Y, 32, 32), new Rectangle(0, 0, 32, 32), Color.White);
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X + 32, (int)_position.Y, _midWidth, 32), new Rectangle(32, 0, 32, 32), Color.White);
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X + _midWidth + 32, (int)_position.Y, 32, 32), new Rectangle(64, 0, 32, 32), Color.White);
+
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X, (int)_position.Y + 32, 32, _midHeight), new Rectangle(0, 32, 32, 32), Color.White);
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X + 32, (int)_position.Y + 32, _midWidth, _midHeight), new Rectangle(32, 32, 32, 32), Color.White);
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X + _midWidth + 32, (int)_position.Y + 32, 32, _midHeight), new Rectangle(64, 32, 32, 32), Color.White);
+
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X, (int)_position.Y + _midHeight + 32, 32, 32), new Rectangle(0, 64, 32, 32), Color.White);
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X + 32, (int)_position.Y + _midHeight + 32, _midWidth, 32), new Rectangle(32, 64, 32, 32), Color.White);
+                spritebatch.Draw(_texture, new Rectangle((int)_position.X + _midWidth + 32, (int)_position.Y + _midHeight + 32, 32, 32), new Rectangle(64, 64, 32, 32), Color.White);
+            }
+        }
+    }
+}
